e8193768143e9b121f75b84e26700f0ee805ac4f
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2023-11-16 00:15:16 -0600

    Add marquee and make some elements responsive

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,6 +8,20 @@ export default {
         acumin: ["Acumin Pro", "sans-serif"],
       },
     },
+    animation: {
+      marquee: "marquee 30s linear infinite",
+      marquee2: "marquee2 30s linear infinite",
+    },
+    keyframes: {
+      marquee: {
+        "0%": { transform: "translateX(0%)" },
+        "100%": { transform: "translateX(-100%)" },
+      },
+      marquee2: {
+        "0%": { transform: "translateX(100%)" },
+        "100%": { transform: "translateX(0%)" },
+      },
+    },
   },
   plugins: [],
 };
