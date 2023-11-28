0f75991e4673278f94cadaadec9ba97355f130ee
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2023-11-27 21:17:55 -0600

    Add event lister to intercept form submit events

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+// intersection observer adds classes as window scrolls
 const myObserver = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     console.log(entry);
@@ -9,3 +10,53 @@ const myObserver = new IntersectionObserver((entries) => {
 
 const myHiddenElements = document.querySelectorAll(".slide_in");
 myHiddenElements.forEach((element) => myObserver.observe(element));
+
+// event listener for email-form intercepting the submit event
+window.addEventListener("load", () => {
+  const form = document.getElementById("email-form") as HTMLFormElement | null;
+
+  if (form) {
+    form.addEventListener("submit", (e: Event) => {
+      e.preventDefault();
+
+      const data = new FormData(form);
+      const action = (e.target as HTMLFormElement).action;
+
+      fetch(action, {
+        method: "POST",
+        body: data,
+      })
+        .then(() => {
+          alert("Success!");
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    });
+  }
+});
+
+// event listener for inquiry-form intercepting the submit event
+window.addEventListener("load", () => {
+  const form = document.getElementById("inquiry-form") as HTMLFormElement | null;
+
+  if (form) {
+    form.addEventListener("submit", (e: Event) => {
+      e.preventDefault();
+
+      const data = new FormData(form);
+      const action = (e.target as HTMLFormElement).action;
+
+      fetch(action, {
+        method: "POST",
+        body: data,
+      })
+        .then(() => {
+          alert("Success!");
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    });
+  }
+});
